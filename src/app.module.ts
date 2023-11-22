a0e6ce2d363a21e14e74cd53f79ecdc24305f3fb
JamesSleep <supercurve2256@gmail.com>
2023-11-22 20:51:21 +0900

    DB 연결설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: Number(configService.get('DATABASE_PORT')),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASS'),
+        database: configService.get('DATABASE_DATA'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
